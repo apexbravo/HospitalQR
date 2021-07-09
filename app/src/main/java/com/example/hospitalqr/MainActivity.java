a4363c75d9262b8ec69af1f36fb7ad1ea9c8f32c
Abel Mugari <abelrevelation@gmail.com>
2021-07-09 18:20:45 +0200

    Added the scanner now it is able to scan the qr codes

--- app/src/main/java/com/example/hospitalqr/MainActivity.java
+++ app/src/main/java/com/example/hospitalqr/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.hospitalqr;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.common.internal.Objects;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -16,6 +31,12 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.frame.Frame;
+import com.otaliastudios.cameraview.frame.FrameProcessor;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
     boolean isDetected = false;
     Button mButtonStart_Again;
 
-    FirebaseVisionBarcodeDetectorOptions options;
+  FirebaseVisionBarcodeDetectorOptions options;
     FirebaseVisionBarcodeDetector mDetector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse response) {
+                        setupCamera();
 
                     }
 
@@ -48,5 +70,89 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 }).check();
+    }
+
+    private void setupCamera() {
+        mButtonStart_Again = (Button) findViewById(R.id.btn_again);
+        mButtonStart_Again.setEnabled(isDetected);
+        mButtonStart_Again.setOnClickListener(v -> isDetected = !isDetected);
+        mCameraView =(CameraView) findViewById(R.id.cameaview);
+        mCameraView.setLifecycleOwner(this);
+        mCameraView.addFrameProcessor(frame -> processImage(getVisionImageFromFrame(frame)));
+
+        options = new FirebaseVisionBarcodeDetectorOptions.Builder()
+                .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE).build();
+        mDetector = FirebaseVision.getInstance().getVisionBarcodeDetector(options);
+    }
+
+    private void processImage(FirebaseVisionImage image) {
+        if(!isDetected){
+          //  Toast.makeText(MainActivity.this,"Detecitng ", Toast.LENGTH_LONG).show();
+            mDetector.detectInImage(image)
+                    .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                        @Override
+                        public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                            processResult(firebaseVisionBarcodes);
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(MainActivity.this,"" + e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
+    private void processResult(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+        if(firebaseVisionBarcodes.size()>0)
+        {
+            isDetected = true;
+            mButtonStart_Again.setEnabled(isDetected);
+            for(FirebaseVisionBarcode item: firebaseVisionBarcodes)
+            {
+                Rect bounds = item.getBoundingBox();
+                Point[] courners = item.getCornerPoints();
+
+                int value_type = item.getValueType();
+                switch (value_type){
+                    case FirebaseVisionBarcode.TYPE_TEXT:
+                        createDialog(item.getRawValue());
+                        break;
+
+                    case FirebaseVisionBarcode.TYPE_URL: {
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(item.getRawValue()));
+                        startActivity(intent);
+                    }
+                    break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+    }
+
+    private void createDialog(String text) {
+        AlertDialog.Builder  builder = new AlertDialog.Builder(this);
+        builder.setMessage(text)
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    private FirebaseVisionImage getVisionImageFromFrame(Frame frame) {
+        byte[] data = frame.getData();
+        FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
+                .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                .setHeight(frame.getSize().getHeight())
+                .setWidth(frame.getSize().getWidth())
+                .build();
+        return FirebaseVisionImage.fromByteArray(data,metadata);
     }
 }
