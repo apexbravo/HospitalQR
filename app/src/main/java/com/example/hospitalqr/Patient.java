9d2713ad0858bf5dddb1ec33330e9e4822ef98f4
Abel Mugari <abelrevelation@gmail.com>
2021-07-10 21:20:31 +0200

    Linked the activity to the cloud database

---
+++ app/src/main/java/com/example/hospitalqr/Patient.java
@@ -0,0 +1,61 @@
+package com.example.hospitalqr;
+
+public class Patient {
+    String Pat_Address;
+    String Pat_Diagnosed;
+    int Pat_ID;
+    String Pat_Name;
+    String Pat_Surname;
+    String Pat_PhoneNumber;
+
+    public Patient() {
+    }
+
+    public String getPat_Address() {
+        return Pat_Address;
+    }
+
+    public void setPat_Address(String pat_Address) {
+        Pat_Address = pat_Address;
+    }
+
+    public String getPat_Diagnosed() {
+        return Pat_Diagnosed;
+    }
+
+    public void setPat_Diagnosed(String pat_Diagnosed) {
+        Pat_Diagnosed = pat_Diagnosed;
+    }
+
+    public int getPat_ID() {
+        return Pat_ID;
+    }
+
+    public void setPat_ID(int pat_ID) {
+        Pat_ID = pat_ID;
+    }
+
+    public String getPat_Name() {
+        return Pat_Name;
+    }
+
+    public void setPat_Name(String pat_Name) {
+        Pat_Name = pat_Name;
+    }
+
+    public String getPat_Surname() {
+        return Pat_Surname;
+    }
+
+    public void setPat_Surname(String pat_Surname) {
+        Pat_Surname = pat_Surname;
+    }
+
+    public String getPat_PhoneNumber() {
+        return Pat_PhoneNumber;
+    }
+
+    public void setPat_PhoneNumber(String pat_PhoneNumber) {
+        Pat_PhoneNumber = pat_PhoneNumber;
+    }
+}
